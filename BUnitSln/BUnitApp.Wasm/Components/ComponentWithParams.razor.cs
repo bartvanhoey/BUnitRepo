1e8ff484984cf5768e2de1ae7ed76d5a3e837e98
ekapituro <ekapituro@gmail.com>
2025-02-04 20:48:50 +0100

    update

---
+++ BUnitSln/BUnitApp.Wasm/Components/ComponentWithParams.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BUnitApp.Wasm.Components;
+
+public partial class ComponentWithParams : ComponentBase
+{
+    [Parameter] public int Numbers { get; set; }
+    [Parameter] public List<string>? Lines { get; set; }
+}
---
+++ BUnitSln/BunitApp.Tests/ComponentWithParamsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BUnitApp.Wasm.Components;
+
+namespace BUnitApp.Tests;
+
+public class ComponentWithParamsTests : BunitContext
+{
+    [Fact]
+    public void CounterStartsAtZero()
+    {
+        
+        var lines = new List<string> { "line1", "line2" };
+        
+        
+        
+        // Arrange
+        var cut = Render<ComponentWithParams>(parameters => parameters.Add(p => p.Numbers, 10).Add(p => p.Lines, lines));
+
+        // Assert that content of the paragraph shows counter at zero
+        cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
+    }
+}
