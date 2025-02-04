2495f7e2d1068fb3779c3f3b714d5dcb9763a8fa
ekapituro <ekapituro@gmail.com>
2025-02-04 20:29:47 +0100

    Update

---
+++ BUnitSln/BunitApp.Tests/HelloWorld/HelloWorldTests.cs
@@ -0,0 +1,14 @@
+﻿namespace BUnitApp.Tests.HelloWorld;
+
+public class HelloWorldTests : BunitContext
+{
+    [Fact]
+    public void HelloWorldComponentRendersCorrectly()
+    {
+        
+        var cut = Render<BUnitApp.Wasm.Components.HelloWorld>();
+
+        
+        cut.MarkupMatches("<h1>Hello world from Blazor</h1>");
+    }
+}
